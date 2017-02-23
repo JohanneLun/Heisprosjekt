5c1496484503bbcd68d21d136b66efb2ee87a8fa
Student <student@itk-d650>
2017-02-23 21:53:42 +0100

    laget queue manager filer (.c og .h) og lagt funksjonene fra klassediagrammet i begge filene uten å implementere noen av dem, og endret navnet til state machine filene bittelitt

---
+++ QueueManager.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+void add_order_in_Q(){
+
+}
+
+
+void get_next_in_Q(){
+
+}
+
+void compare_Q_and_floor(){
+
+}
+
+void delete_Q(){
+	
+}
---
+++ QueueManager.h
@@ -0,0 +1,26 @@
+
+
+//a queue element with the value and a pointer to the next queue element
+struct Queue_Element {
+	int motor_direction;
+	struct Queue_Element *next;
+}
+
+
+
+//functions
+
+//depending on the order, the function will add a 1,0 or -1 to the queue.
+void add_order_in_Q();
+
+// retrieves the next queue element/the next direction of the motor
+// should this be a static???
+void get_next_in_Q();
+
+//compares the current floor and direction to the order and adds accordingly to te queue
+// should this be in the state machine file??
+void compare_Q_and_floor();
+
+//deletes the entire queue and sets the first queue element to a null-pointer
+void delete_Q();
+
