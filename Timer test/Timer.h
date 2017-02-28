0309a412a23c08f13c2ff697a38aaf57ae56e1d2
Student <elsa.ariansen@gmail.com>
2017-02-28 13:38:16 +0100

    lagt til noen kommentarer i h-filen

--- Timer test/Timer.h
+++ Timer test/Timer.h
@@ -5,6 +5,11 @@ struct timer_struct{
 	double initial_time;
 };
 
+// utlevert funksjon som henter ant sekunder siden 1.jan 1970
 double get_wall_time();
+
+//starter timeren, setter initial_time lik get_wall_time()
 void start_timer(struct timer_struct* timer);
+
+//Returnerer 1 hvis det har gått 3 sekunder, og 0 ellers
 int check_time(struct timer_struct timer);
